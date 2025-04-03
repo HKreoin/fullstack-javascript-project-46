ac047a928c33e121714f0767551669ac41d630d5
Ilshat Khabirov <khabdev@yandex.ru>
2025-04-03 13:17:22 +0300

    добавил файлы для сравнения и логику сравнения json

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,18 @@
 #!/usr/bin/env node
+import { Command } from 'commander';
+import genDiff from '../src/index.js';
+
+const program = new Command();
+
+program
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1>', 'path to first file')
+  .argument('<filepath2>', 'path to second file')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2, options) => {
+    const diff = genDiff(filepath1, filepath2);
+    console.log(diff);
+  });
+
+program.parse(process.argv);
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import path from 'path';
+
+const getFileContent = (filepath) => {
+  const absolutePath = path.resolve(filepath);
+  const content = fs.readFileSync(absolutePath, 'utf8');
+  return JSON.parse(content);
+};
+
+const genDiff = (filepath1, filepath2) => {
+  const obj1 = getFileContent(filepath1);
+  const obj2 = getFileContent(filepath2);
+  
+  const allKeys = [...new Set([...Object.keys(obj1), ...Object.keys(obj2)])].sort();
+  
+  const result = allKeys.map(key => {
+    if (!(key in obj1)) {
+      return `  + ${key}: ${obj2[key]}`;
+    }
+    if (!(key in obj2)) {
+      return `  - ${key}: ${obj1[key]}`;
+    }
+    if (obj1[key] !== obj2[key]) {
+      return `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`;
+    }
+    return `    ${key}: ${obj1[key]}`;
+  });
+  
+  return `{\n${result.join('\n')}\n}`;
+};
+
+export default genDiff;
